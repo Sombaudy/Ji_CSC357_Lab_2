c8b23945cf5c1e57dc6dc2599e30611f6d06fdd0
Sombaudy <jhji@unix3.csc.calpoly.edu>
2023-10-11 15:44:56 -0700

    completed

---
+++ command_5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+    
+    for (int i=0; i<argc; i++) {
+        if (argv[i][0] == '-') {
+            printf("%s\n", argv[i]);
+        }
+    }
+
+    return 0;
+}
